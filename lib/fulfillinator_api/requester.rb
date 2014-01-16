34e60daf04ff3757e1d297f190fc336f931390eb
Tyrel Richey <tyrelrichey@gmail.com>
2014-01-16 14:58:19 -0700

    JKL

--- lib/fulfillinator_api/requester.rb
+++ lib/fulfillinator_api/requester.rb
@@ -10,7 +10,10 @@ module FulfillinatorApi
 			end
 			def issue_request( action_string, params )
 				uri = URI.parse(full_uri(action_string))
-				response = Net::HTTP.post_form(uri, params)
+				req = Net::HTTP::Post.new(uri.path)
+				req.use_ssl = true
+				req.form_data(params)
+				Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
 			end
 		end
 	end
