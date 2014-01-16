d23dc3a0f4404f375f5259fb28516e44ec713965
Tyrel Richey <tyrelrichey@gmail.com>
2014-01-16 10:46:33 -0700

    JKL

--- lib/fulfillinator_api/requester.rb
+++ lib/fulfillinator_api/requester.rb
@@ -2,8 +2,8 @@ module FulfillinatorApi
 	module Requester
 		class << self
 			def base_uri
-				"http://localhost:3000/api/v1/"
-				# "https://fulfillinator.com/api/v1/"
+				# "http://localhost:3000/api/v1/"
+				"https://fulfillinator.com/api/v1/"
 			end
 			def full_uri( action_string )
 				"#{base_uri}#{action_string}"
